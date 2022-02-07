415f45f0ef1d1ed68966b0a9ef21de7baa43d2b8
Vingnir <vingnir@live.se>
2022-02-07 23:36:14 +0100

    added commands

--- golf.sql
+++ golf.sql
@@ -1,8 +1,6 @@
---- Lexicon SQL Golf Cup ---
+--  Lexicon SQL Golf Cup --
 -- drop database golf;
-create database golf;
-use database golf;
-
+USE golf;
 CREATE TABLE `player` (
   `pnr` varchar(13),
   `name` varchar(30),
@@ -10,25 +8,21 @@ CREATE TABLE `player` (
   PRIMARY KEY (`pnr`)
 ) ENGINE=InnoDB; 
 
-CREATE TABLE `competition` (
-  `competition_name` varchar(13),
-  `date` varchar(30),
-  PRIMARY KEY (`competition_name`)
-) ENGINE=InnoDB;  
---  CREATE INDEX index_name ON table_name (column1, column2, ...);
--- ALTER TABLE `table` ADD UNIQUE INDEX `name` (`username`, `date`);
- 
 CREATE TABLE `jacket` (
   `initials` varchar(3),
   `size` varchar(10),
   `material` varchar(10),
-  PRIMARY KEY (`initials`)
+  `pnr` varchar(13),
+  PRIMARY KEY (`pnr`,`initials`),
+  FOREIGN KEY(`pnr`) references player(pnr)
 ) ENGINE=InnoDB;  
 
 CREATE TABLE `club` (
   `nr` int,
   `material` varchar(30),
-  PRIMARY KEY (`nr`)
+  `pnr` varchar(13),
+  PRIMARY KEY (`pnr`,`nr`),
+  foreign KEY(`pnr`) references player(pnr)
 ) ENGINE=InnoDB;  
 
 CREATE TABLE `construction` (
@@ -37,20 +31,17 @@ CREATE TABLE `construction` (
   PRIMARY KEY (`serial_nr`)
 ) ENGINE=InnoDB;  
 
+CREATE TABLE `competition` (
+  `competition_name` varchar(13),
+  `date` varchar(30),
+  PRIMARY KEY (`competition_name`)
+) ENGINE=InnoDB;  
+
+--  CREATE INDEX index_name ON table_name (column1, column2, ...);
+-- ALTER TABLE `table` ADD UNIQUE INDEX `name` (`username`, `date`);
+
 CREATE TABLE `rain` (
   `type` varchar(30),
   `wind_strenght` varchar(30),
   PRIMARY KEY (`type`)
-) ENGINE=InnoDB;  
-
---joined tables
-CREATE TABLE `participate` (
-  `player` varchar(30),
-  `competition` varchar(30),
-) ENGINE=InnoDB;  
-
-CREATE TABLE `has` (
-  `rain` varchar(30),
-  `competition` varchar(30),
-  `time` varchar(10),
 ) ENGINE=InnoDB;  
